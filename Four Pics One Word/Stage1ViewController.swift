d6189ca534e70f371cdf5089134e3786b5ed9442
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-04-15 10:38:22 -0500

    Set up Stage 1 view controller

---
+++ Four Pics One Word/Stage1ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Stage1ViewController.swift
+//  Four Pics One Word
+//
+//  Created by Student on 4/15/21.
+//
+
+import UIKit
+
+class Stage1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
